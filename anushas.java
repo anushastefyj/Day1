af8476f82c36d2b9c85718a4fd51181dbc745c66
anushastefyj <24hr5a3001@mtieat.org>
2024-12-05 16:15:55 +0530

    Add files via upload

---
+++ anushas.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class anushas
+{
+    public static void main(String args[])
+    {
+        int a=12;
+        char b='C';
+        float c=3.14f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
